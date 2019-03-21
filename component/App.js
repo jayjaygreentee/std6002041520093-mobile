7d853f60037ac1917af5786fb4c8500ac6e0b383
ระพีพันธ์ นาคพันธ์ <s6002041520093@Macmini-213-23.local>
2019-03-21 13:21:42 +0700

    newcomponent

---
+++ component/App.js
@@ -0,0 +1,17 @@
+//import Library
+import React from 'react';
+import { View , Text } from 'react-native';
+
+//vrite component
+class App extends React.Component {
+    render() {
+        return (
+
+            <View>
+                <Text>Rapepan</Text>
+            </View>
+        )
+    }
+}
+
+export default App;
