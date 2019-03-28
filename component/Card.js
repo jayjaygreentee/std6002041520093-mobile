70262d4f62d39aff0f39dae45130c54e53828d7d
ระพีพันธ์ นาคพันธ์ <s6002041520093@Macmini-213-23.local>
2019-03-28 15:45:05 +0700

    ใส่รูปใน card.js

--- component/Card.js
+++ component/Card.js
@@ -6,6 +6,13 @@ class Card extends Component {
     render() {
         return (
             <View>
+                <CardSection>
+                <Image 
+                    style={{width:300, height:300}}
+                    source={{uri:'https://images.unsplash.com/photo-1546717137-f348ef0e0e3d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80' }} />
+
+                </CardSection>
+                
                 <CardSection />
                 <CardSection />
                 <CardSection />
--- component/CardSection.js
+++ component/CardSection.js
@@ -5,9 +5,7 @@ class CardSection extends Component {
     render() {
         return (
             <View>
-                <Image 
-                    style={{width:50, height:50}}
-                    source={{uri:'https://images.unsplash.com/photo-1546717137-f348ef0e0e3d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80' }} />
+                {this.props.children}
                 <Text>123456</Text>
             </View>
 
