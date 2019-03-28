d25bde414ae5712c7dcd1360209396671d1cfb27
ระพีพันธ์ นาคพันธ์ <s6002041520093@Macmini-213-23.local>
2019-03-28 15:38:54 +0700

    ใส่รูป

--- component/Card.js
+++ component/Card.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import CardSection from './CardSection';
 
 class Card extends Component {
--- component/CardSection.js
+++ component/CardSection.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 class CardSection extends Component {
     render() {
         return (
             <View>
+                <Image 
+                    style={{width:50, height:50}}
+                    source={{uri:'https://images.unsplash.com/photo-1546717137-f348ef0e0e3d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80' }} />
                 <Text>123456</Text>
             </View>
 
